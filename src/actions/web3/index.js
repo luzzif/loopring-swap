7097bbee18e05cb07db75090cf38e7eb616eaa88
luzzif <fedeluzzi00@gmail.com>
2020-09-05 00:07:38 +0200

    feat: logout on metamask chain or account change

--- src/actions/web3/index.js
+++ src/actions/web3/index.js
@@ -1,5 +1,6 @@
 import { getWeb3Modal } from "../../views/app";
 import Web3 from "web3";
+import { logout } from "../loopring";
 
 export const INITIALIZE_WEB3_SUCCESS = "INITIALIZE_WEB3_SUCCESS";
 export const WEB3_ACCOUNT_CHANGED = "WEB3_ACCOUNT_CHANGED";
@@ -8,15 +9,13 @@ export const initializeWeb3 = () => async (dispatch) => {
     try {
         const web3Modal = getWeb3Modal();
         const provider = await web3Modal.connect();
-        provider.on("accountsChanged", (accounts) => {
-            if (accounts.length > 0) {
-                dispatch({
-                    type: WEB3_ACCOUNT_CHANGED,
-                    selectedAccount: accounts[0],
-                });
-            }
-        });
         provider.autoRefreshOnNetworkChange = false;
+        provider.on("chainChanged", () => {
+            dispatch(logout());
+        });
+        provider.on("accountsChanged", () => {
+            dispatch(logout());
+        });
         const web3 = new Web3(provider);
         const accounts = await web3.eth.getAccounts();
         dispatch({
