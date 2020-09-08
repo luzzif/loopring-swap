4294dc0b6019bcb9b75ba3bef43b1987236aa8ec
luzzif <fedeluzzi00@gmail.com>
2020-09-08 22:17:38 +0200

    feat: get icon images from loopring.io

--- src/components/token-icon/index.jsx
+++ src/components/token-icon/index.jsx
@@ -19,7 +19,7 @@ export const TokenIcon = ({ address, size }) => {
             }
             default: {
                 // If the address is ETH, we simply fallback to the WETH icon.
-                return `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${
+                return `https://loopring.io/assets/images/ethereum/assets/${
                     address === "0x0000000000000000000000000000000000000000"
                         ? "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
                         : address
