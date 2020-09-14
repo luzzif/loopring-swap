a29457365b4be5158d94b92853ef5fb15450c3cf
luzzif <fedeluzzi00@gmail.com>
2020-09-14 19:08:42 +0200

    feat: add weitoether and ethertowei functions for unit conversion

--- src/utils/index.js
+++ src/utils/index.js
@@ -12,9 +12,15 @@ export const getLoopringApiKey = async (wallet, account) => {
     );
 };
 
-
 export const getShortenedEthereumAddress = (address) =>
     `${address.substring(0, 5)}...${address.substring(
         address.length - 4,
         address.length
     )}`;
+
+export const weiToEther = (weiBigNumber, decimals) => {
+    return weiBigNumber.dividedBy("1e" + decimals);
+};
+
+export const etherToWei = (etherBigNumber, decimals) =>
+    etherBigNumber.multipliedBy("1e" + decimals);
